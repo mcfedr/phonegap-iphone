8de76fa9c15140e5174dd2c9317b52aa75588869
Fred Cox <fred@ekreative.com>
2011-10-07 11:32:08 +0300

    Update filetransfer to use ASIHTTPRequest
    This means that file uploads are streamed from disk, not loaded into memory
    which caused crashes when trying to upload larger files, particularly videos

--- PhoneGapLib/Classes/FileTransfer.m
+++ PhoneGapLib/Classes/FileTransfer.m
@@ -8,24 +8,26 @@
  */
 
 #import "FileTransfer.h"
-
+#import "ASIFormDataRequest.h"
 
 @implementation PGFileTransfer
 
 - (void) upload:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options {
     NSString* callbackId = [arguments objectAtIndex:0];
     NSString* fileKey = (NSString*)[options objectForKey:@"fileKey"];
-    NSString* fileName = (NSString*)[options objectForKey:@"fileName"];
-    NSString* mimeType = (NSString*)[options objectForKey:@"mimeType"];
+    //NSString* fileName = (NSString*)[options objectForKey:@"fileName"];
+    //NSString* mimeType = (NSString*)[options objectForKey:@"mimeType"];
     NSMutableDictionary* params = [NSMutableDictionary dictionaryWithDictionary:(NSDictionary*)[options objectForKey:@"params"]];
     NSString* filePath = (NSString*)[options objectForKey:@"filePath"];
     NSString* server = (NSString*)[options objectForKey:@"server"];
+    NSString* progressCallback = (NSString*)[options objectForKey:@"progressCallback"];
+    
     PluginResult* result = nil;
     FileTransferError errorCode = 0;
-
+    
     
     NSURL* file;
-    NSData *fileData = nil;
+    //NSData *fileData = nil;
     
     if ([filePath hasPrefix:@"/"]) {
         file = [NSURL fileURLWithPath:filePath];
@@ -51,12 +53,12 @@
             errorCode = FILE_NOT_FOUND_ERR;
         } else {
             // file exists, make sure we can get the data
-            fileData = [NSData dataWithContentsOfURL:file];
-            
-            if(!fileData) {
-                errorCode =  FILE_NOT_FOUND_ERR;
-                NSLog(@"File Transfer Error: Could not read file data");
-            }
+            /*fileData = [NSData dataWithContentsOfURL:file];
+             
+             if(!fileData) {
+             errorCode =  FILE_NOT_FOUND_ERR;
+             NSLog(@"File Transfer Error: Could not read file data");
+             }*/
         }
         [fileMgr release];
     }
@@ -67,36 +69,28 @@
         return;
     }
     
-    NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url];
-	[req setHTTPMethod:@"POST"];
-	
-//    Magic value to set a cookie
-	if([params objectForKey:@"__cookie"]) {
-		[req setValue:[params objectForKey:@"__cookie"] forHTTPHeaderField:@"Cookie"];
-		[params removeObjectForKey:@"__cookie"];
-		[req setHTTPShouldHandleCookies:NO];
-	}
-	
-	NSString *boundary = @"*****com.phonegap.formBoundary";
+    ASIFormDataRequest *request = [ASIFormDataRequest requestWithURL:url];
+    [request setRequestMethod:@"POST"];
     
-	NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
-	[req setValue:contentType forHTTPHeaderField:@"Content-Type"];
-    //Content-Type: multipart/form-data; boundary=*****com.phonegap.formBoundary
-	[req setValue:@"XMLHttpRequest" forHTTPHeaderField:@"X-Requested-With"];
-	NSString* userAgent = [[self.webView request] valueForHTTPHeaderField:@"User-agent"];
+    
+    if([params objectForKey:@"__cookie"]) {
+        [request setUseCookiePersistence:NO];
+        [request setRequestCookies:[params objectForKey:@"__cookie"]];
+        [params removeObjectForKey:@"__cookie"];
+    }
+    
+    [request addRequestHeader:@"X-Requested-With" value:@"XMLHttpRequest"];
+    NSString* userAgent = [[self.webView request] valueForHTTPHeaderField:@"User-agent"];
 	if(userAgent) {
-		[req setValue: userAgent forHTTPHeaderField:@"User-Agent"];
+		[request addRequestHeader:@"User-agent" value:userAgent];
 	}
-
     
-	NSMutableData *postBody = [NSMutableData data];
-	
-	NSEnumerator *enumerator = [params keyEnumerator];
+    NSEnumerator *enumerator = [params keyEnumerator];
 	id key;
 	id val;
 	
 	while ((key = [enumerator nextObject])) {
-		val = [params objectForKey:key];
+        val = [params objectForKey:key];
 		if(!val || val == [NSNull null]) {
 			continue;	
 		}
@@ -108,30 +102,22 @@
 		if (![val isKindOfClass:[NSString class]]) {
 			continue;
 		}
-		
-		[postBody appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-		[postBody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n", key] dataUsingEncoding:NSUTF8StringEncoding]];
-		[postBody appendData:[val dataUsingEncoding:NSUTF8StringEncoding]];
-		[postBody appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-	}
+        [request setPostValue:val forKey:key];
+    }
     
-	[postBody appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-	[postBody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"; filename=\"%@\"\r\n", fileKey, fileName] dataUsingEncoding:NSUTF8StringEncoding]];
-    [postBody appendData:[[NSString stringWithFormat:@"Content-Type: %@\r\n\r\n", mimeType] dataUsingEncoding:NSUTF8StringEncoding]];
-    NSLog(@"fileData length: %d", [fileData length]);
-	[postBody appendData:fileData];
-	[postBody appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+    [request setFile:[file path] forKey:fileKey];
     
-    //[req setValue:[[NSNumber numberWithInteger:[postBody length]] stringValue] forHTTPHeaderField:@"Content-Length"];
-	[req setHTTPBody:postBody];
     
-	
-	FileTransferDelegate* delegate = [[[FileTransferDelegate alloc] init] autorelease];
+    FileTransferDelegate* delegate = [[FileTransferDelegate alloc] init];
 	delegate.command = self;
     delegate.callbackId = callbackId;
-	
-	[NSURLConnection connectionWithRequest:req delegate:delegate];
+    delegate.progressCallback = progressCallback;
     
+    [request setDelegate:delegate];
+    request.showAccurateProgress = YES;
+    [request setUploadProgressDelegate:delegate];
+    
+    [request startAsynchronous];
 }
 
 @end
@@ -139,70 +125,54 @@
 
 @implementation FileTransferDelegate
 
-@synthesize callbackId, responseData, command, bytesWritten;
+@synthesize callbackId, responseData, command, bytesWritten, progressCallback;
 
 
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection 
-{
-    NSString* response = [[NSString alloc] initWithData:self.responseData encoding:NSUTF8StringEncoding];
-    // create dictionary to return FileUploadResult object
-    NSMutableDictionary* uploadResult = [NSMutableDictionary dictionaryWithCapacity:3];
-    [uploadResult setObject: [response stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding] forKey: @"response"];
-    [uploadResult setObject:[NSNumber numberWithInt: self.bytesWritten] forKey:@"bytesSent"];
-    [uploadResult setObject:[NSNull null] forKey: @"responseCode"];
-    PluginResult* result = [PluginResult resultWithStatus: PGCommandStatus_OK messageAsDictionary: uploadResult cast: @"navigator.fileTransfer._castUploadResult"];
-    [command writeJavascript:[result toSuccessCallbackString: callbackId]];
-    [response release];
-}
-
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error 
-{
-    PluginResult* result = [PluginResult resultWithStatus: PGCommandStatus_OK messageAsInt: CONNECTION_ERR cast: @"navigator.fileTransfer._castTransferError"];
-    NSLog(@"File Transfer Error: %@", [error localizedDescription]);
-    [command writeJavascript:[result toErrorCallbackString: callbackId]];
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
-{
-    [responseData appendData:data];
-}
-- (void)connection:(NSURLConnection *)connection didSendBodyData:(NSInteger)bytesWritten totalBytesWritten:(NSInteger)totalBytesWritten totalBytesExpectedToWrite:(NSInteger)totalBytesExpectedToWrite
-{
-    self.bytesWritten = totalBytesWritten;
-}
-/* TESTING ONLY CODE
-// use ONLY for testing with self signed certificates
-// uncomment and modify server name in connectiondidReceiveAuthenticationChallenge
-- (BOOL)connection:(NSURLConnection *)connection canAuthenticateAgainstProtectionSpace:(NSURLProtectionSpace *)protectionSpace
-{
-    return [protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust];
-}
-
-- (void)connection:(NSURLConnection *) connection didReceiveAuthenticationChallenge:(NSURLAuthenticationChallenge*)challenge
-{
-	if ([challenge.protectionSpace.authenticationMethod isEqualToString:NSURLAuthenticationMethodServerTrust])
-	{
-        //NSLog(@"challenge host: %@", challenge.protectionSpace.host);
-		// we only trust our own domain
-		if ([challenge.protectionSpace.host isEqualToString:@"serverName.domain.com"]){
-            NSURLCredential* myCredential = [NSURLCredential credentialForTrust: challenge.protectionSpace.serverTrust];
-            
-            [challenge.sender useCredential:myCredential forAuthenticationChallenge:challenge];
-			
-		}
-	}
-    
-	[challenge.sender continueWithoutCredentialForAuthenticationChallenge:challenge];
-}
-// uncomment the above two methods for testing servers with self signed certificates
-// END TESTING ONLY CODE
- */
 - (id) init
 {
     if ((self = [super init])) {
+        self.bytesWritten = 0;
 		self.responseData = [NSMutableData data];
     }
     return self;
+}
+
+- (void)requestFinished:(ASIHTTPRequest *)request
+{
+    
+    // Use when fetching text data
+    NSString *responseString = [request responseString];
+    
+    NSMutableDictionary* uploadResult = [NSMutableDictionary dictionaryWithCapacity:3];
+    [uploadResult setObject:[responseString stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding] forKey: @"response"];
+    [uploadResult setObject:[NSNumber numberWithInt: self.bytesWritten] forKey:@"bytesSent"];
+    [uploadResult setObject:[NSNumber numberWithInt: request.responseStatusCode] forKey: @"responseCode"];
+    PluginResult* result = [PluginResult resultWithStatus: PGCommandStatus_OK messageAsDictionary: uploadResult cast: @"navigator.fileTransfer._castUploadResult"];
+    [command writeJavascript:[result toSuccessCallbackString: callbackId]];
+    [self autorelease];
+}
+
+- (void)requestFailed:(ASIHTTPRequest *)request
+{
+    
+    NSError *error = [request error];
+    PluginResult* result = [PluginResult resultWithStatus: PGCommandStatus_OK messageAsInt: CONNECTION_ERR cast: @"navigator.fileTransfer._castTransferError"];
+    NSLog(@"File Transfer Error: %@", [error localizedDescription]);
+    [command writeJavascript:[result toErrorCallbackString: callbackId]];
+    [self autorelease];
+}
+
+- (void)setProgress:(float)newProgress {
+    if(self.progressCallback != nil) {
+        NSMutableDictionary* uploadResult = [NSMutableDictionary dictionaryWithCapacity:1];
+        [uploadResult setObject:[NSNumber numberWithFloat:(newProgress * 100)] forKey:@"percent"];
+        PluginResult* result = [PluginResult resultWithStatus: PGCommandStatus_OK messageAsDictionary: uploadResult cast: @"navigator.fileTransfer._castProgress"];
+        [command writeJavascript:[result toSuccessCallbackString: self.progressCallback]];
+    }
+}
+
+- (void)request:(ASIHTTPRequest *)request didSendBytes:(long long)bytes {
+    self.bytesWritten += bytes;
 }
 
 - (void) dealloc
@@ -210,9 +180,8 @@
     [callbackId release];
 	[responseData release];
 	[command release];
+    [progressCallback release];
     [super dealloc];
 }
 
-
 @end;
-
